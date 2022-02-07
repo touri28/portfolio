b4aa6559d5db0e36e7372e8c34598b9c67774c84
touri anis <anis-0542@hotmail.com>
2022-02-07 01:43:49 +0100

    Add files via upload

---
+++ cv/script/script.js
@@ -0,0 +1,35 @@
+$(function(){
+	$(".navbar a, footer a").on("click", function(event){
+		event.preventDefault();
+		var hash = this.hash;
+		$ ("body").animate({scrolltop : $(hash).offset().top}, 900 ,function(){
+			window.location.hash = hash;})
+	});
+		$('#contact-form').submit(function(e){
+		e.preventDefault();
+		$('.comment').empty();
+		var postdata=$('#contact-form').serialize();
+		$.ajax({
+			type: 'POST',
+			url: 'php/contact.php',
+			data: postdata,
+			dataType: 'json',
+			success: function(resultat){
+				if(resultat.issucces)
+					{
+						$("#contact-form").append("<p class='bien'>votre message a ete bien envoyer,merci de m'avoir contactez :) </p>");
+						$("#contact-form")[0].reset();
+					}
+				else
+				{
+					$("#prenom + .comment").html(resultat.prenomerror);
+					$("#nom + .comment").html(resultat.nomerror);
+					$("#message + .comment").html(resultat.messageerror);
+					$("#email + .comment").html(resultat.emailerror);
+					$("#phone + .comment").html(resultat.phoneerror);
+					
+				}
+			}
+		});
+	});
+})
